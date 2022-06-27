affb68227d9636a62285b8d4103037ba4459a597
Parth Vyas <parthv700@gmail.com>
2022-06-27 18:37:13 +0530

    Done with it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
+import React,{ useState } from 'react';
 import Navbar from './components/Navbar';
 import Textbox from './components/Textbox';
+import About from './components/About';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const [style, setStyle] = useState("light");
+  const toggleStyle = ()=> {
+    if(style === 'light'){
+      setStyle('dark');
+      document.body.style.backgroundColor='#363537';
+      document.body.style.color='#FAFAFA';
+    }
+    else{
+      setStyle('light');
+      document.body.style.backgroundColor='#FFF';
+      document.body.style.color='#363537';
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils"/>
-    <Textbox/>
+    <BrowserRouter>
+    <Navbar title="TextUtils" style={style} toggleStyle={toggleStyle}/>
+      <Routes>
+        <Route path="/"element={<Textbox style={style} toggleStyle={toggleStyle}/>}/>
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 
-export default function Textbox() {
+export default function Textbox(props) {
   const [text, changeText] = useState('');
   const handlerOnChange = (event) => {
     changeText(event.target.value)
@@ -13,17 +13,28 @@ export default function Textbox() {
     let newText=text.toLowerCase();
     changeText(newText);
   }
+  const handlerOnClick3 = () => {
+    let copyText = document.getElementById("floatingTextarea");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    navigator.clipboard.writeText(copyText.value);
+  }
+  const handlerOnClick4 = () => {
+    changeText("");
+  }
   return (
     <div className="container">
-    <div className="form-floating my-3">
-        <textarea className="form-control" value={text} onChange={handlerOnChange} id="floatingTextarea"></textarea>
-        <label htmlFor="floatingTextarea">Your text</label>
-        <button type="button" className="btn btn-primary mx-2 my-2" onClick={handlerOnClick1}>Change to UPPERCASE</button>
-        <button type="button" className="btn btn-primary mx-2 my-2" onClick={handlerOnClick2}>Change to lowercase</button>
+    <div className="form-floating mx-5 my-5">
+        <textarea className="form-control" placeholder="Your text" value={text} onChange={handlerOnChange} id="floatingTextarea" style={{height: "100px"}}></textarea>
+        <label htmlFor="floatingTextarea" style={{color: props.style === "light" ? "#363537" : "#FFFFF"}}>Your text</label>
+        <button type="button" className={`btn btn-${props.style} mx-2 my-2`} onClick={handlerOnClick1} disabled={text.length===0}>Change to UPPERCASE</button>
+        <button type="button" className={`btn btn-${props.style} mx-2 my-2`} onClick={handlerOnClick2} disabled={text.length===0}>Change to lowercase</button>
+        <button type="button" className={`btn btn-${props.style} mx-2 my-2`} onClick={handlerOnClick3} disabled={text.length===0}>Copy to clipboard</button>
+        <button type="button" className={`btn btn-${props.style} mx-2 my-2`} onClick={handlerOnClick4} disabled={text.length===0}>Reset</button>
     </div>
     <h2>Summary</h2>
-    <p>{text.length} characters, {text.split(" ").length} words</p>
-    <iframe title="quote" src="https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=ffffff&fid=0"></iframe>
+    <p>{text.length} characters, {text.split(/\s+/).filter((ele)=>{return ele.length!==0}).length} words</p>
+    <iframe frameBorder="0" title="quote" src={`${props.style === "light" ?"https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=000000&background=ffffff": "https://kwize.com/quote-of-the-day/embed/&txt=0&font=&color=fafafa&background=080808"}`}></iframe>
     </div>
   )
 }
