6d41d77865da759df0e2a7665f0d58fbfd8b038b
Parth Vyas <parthv700@gmail.com>
2022-06-28 09:39:25 +0530

    Last tweaks

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,11 @@ export default function About() {
   return (
     <div className="container my-5">
       <h2>TextUtils</h2><p>is a collection of useful text processing utilities. All text tools are simple, free and easy to use. There are no intrusive ads, popups or other garbage. Just text utilities that work right in your browser. And all utilities work exactly the same way — load text, get result.</p>
-      <figure class="text-center my-5">
-        <blockquote class="blockquote">
+      <figure className="text-center my-5">
+        <blockquote className="blockquote">
           <p>“Life is really simple, but men insist on making it complicated.”</p>
         </blockquote>
-        <figcaption class="blockquote-footer">Confucius</figcaption>
+        <figcaption className="blockquote-footer">Confucius</figcaption>
       </figure>
     </div>
   );
